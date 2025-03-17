aa1014aa5092785ef03d9551c631d9b454674233
rrudolf98 <rafiru.102@gmail.com>
2025-03-17 11:57:05 +0100

    New installation

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-DEST='/usr/local'
+DEST_BIN='/usr/sbin'
+DEST_ETC='/etc/illuminanced'
 ROOT=`dirname $0`
 
 if [ -e /etc/systemd/system/illuminanced.service ]; then
@@ -9,23 +10,18 @@ if [ -e /etc/systemd/system/illuminanced.service ]; then
     echo " Done"
 fi
 
-if [ ! -d $DEST/etc ]; then
-    mkdir $DEST/etc || exit "Cannot create $/DEST/etc"
+if [ ! -d $DEST_ETC ]; then
+    mkdir $DEST_ETC || exit "Cannot create $DEST_ETC"
 fi
 
-if [ ! -d $DEST/sbin ]; then
-    mkdir $DEST/sbin || exit "Cannot create $/DEST/sbin"
-fi
-
-if [ -e $DEST/etc/illuminanced.toml ]; then
-    echo "\nBackup old config file:"
-    mv -v $DEST/etc/illuminanced.toml $DEST/etc/illuminanced.toml_`date +%y%m%d_%H%M%S`;
+if [ ! -d $DEST_BIN ]; then
+    mkdir $DEST_BIN || exit "Cannot create $DEST_BIN"
 fi
 
 echo "\nInstall:"
 
-cp -v $ROOT/target/release/illuminanced $DEST/sbin/ || exit 1
-cp -v $ROOT/illuminanced.toml $DEST/etc || exit 1
+cp -v $ROOT/target/release/illuminanced $DEST_BIN || exit 1
+cp -v $ROOT/illuminanced.conf $DEST_ETC  || exit 1
 cp -v illuminanced.service /etc/systemd/system/illuminanced.service || exit 1
 
 echo "\nStart service:"
